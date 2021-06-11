6e8ab26ef1bc193c928367c7c5bd21060cf1c8fb
Mitchell Murphy <mitchell.murphy@agilesof.com>
2021-06-11 15:26:18 -0400

    Created docker-compose.yml and necessary Postgres files

--- src/settings.py
+++ src/settings.py
@@ -11,14 +11,10 @@ SECRET_KEY = config('SECRET_KEY', cast=Secret)
 DATABASES = {
     "postgres": {
         "driver": config("DB_DRIVER", default="postgres"),
-        "host": config("DB_HOST", default="127.0.0.1"),
+        "host": config("DB_HOST"),
         "database": config("POSTGRES_DB", default="fastapi"),
         "user": config("POSTGRES_USER", default=None),
         "password": config("POSTGRES_PASSWORD", cast=Secret, default=None),
         "port": config("DB_PORT", cast=int, default=5432),
-    },
-    "sqlite": {
-        "driver": "sqlite",
-        "database": "test.db"
     }
 }
